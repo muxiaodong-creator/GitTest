5b77540153410d254a66f3359b07c4622e9f691b
muxiaodong-creator <13610791538@163.com>
2020-03-21 21:13:30 +0800

    新增test配置类

---
+++ src/main/java/com/example/demo/test/config.java
@@ -0,0 +1,17 @@
+package com.example.demo.test;
+
+/**
+ * program: demo
+ * <p>
+ * description
+ * <p>
+ * author: muxd
+ * <p>
+ * create: 2020-03-21 21:10
+ **/
+public class config {
+
+    private  String name;
+
+
+}
