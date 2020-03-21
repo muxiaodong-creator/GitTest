38bc17ea69ad91b81df0ccc7a3422de45c646904
muxiaodong-creator <13610791538@163.com>
2020-03-21 22:06:51 +0800

    增加输出

--- src/main/java/com/example/demo/test/helloworld.java
+++ src/main/java/com/example/demo/test/helloworld.java
@@ -10,4 +10,8 @@ package com.example.demo.test;
  * create: 2020-03-21 21:46
  **/
 public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println(111);
+    }
 }
